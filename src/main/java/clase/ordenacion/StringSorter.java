e9ad8260c88ba91ae88fcd1d608a2ea35c260222
rperez <rodrigo.perez@urjc.es>
2024-03-25 17:02:07 +0100

    minor

--- src/main/java/clase/ordenacion/StringSorter.java
+++ src/main/java/clase/ordenacion/StringSorter.java
@@ -7,7 +7,7 @@ public class StringSorter {
     public static void main(String[] args) {
 	String[] a = StdIn.readAllStrings();
 	Insertion.sort(a);
-	for (int i = 0; i < a.length; i++)
-	    StdOut.println(a[i]);
+	for (String s : a)
+		StdOut.println(s);
     }
 }
