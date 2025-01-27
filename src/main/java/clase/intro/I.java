29a29b917c78b8f158c465db8ac09e4eeefbd843
roi <rodrigo.perez@urjc.es>
2025-01-27 12:17:36 +0100

    new basic example

---
+++ src/main/java/clase/intro/I.java
@@ -0,0 +1,16 @@
+package clase.intro;
+
+public class I {
+    public static void main(String[] args) {
+        int i, a;
+        i = 3;
+
+        a = i++;
+        System.out.println("a = " + a);
+        System.out.println("i = " + i);
+        a = ++i;
+        System.out.println("a = " + a);
+        System.out.println("i = " + i);
+
+    }
+}
