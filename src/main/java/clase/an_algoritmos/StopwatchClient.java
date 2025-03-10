4514b7d4c9e850d8287379e814132b1e5ec7f899
roi <rodrigo.perez@urjc.es>
2025-03-10 13:51:12 +0100

    minor

--- src/main/java/clase/an_algoritmos/StopwatchClient.java
+++ src/main/java/clase/an_algoritmos/StopwatchClient.java
@@ -8,10 +8,9 @@ public class StopwatchClient
     {
 	int n = Integer.parseInt(args[0]);
 	int[] a = new int[n];
-	for (int i = 0; i < n; i++)
-	    {
+	for (int i = 0; i < n; i++) {
 		a[i] = StdRandom.uniform(-1000000, 1000000);
-	    }
+	}
 	Stopwatch timer = new Stopwatch();
 	int count = ThreeSum.count(a);
 	double time = timer.elapsedTime();
