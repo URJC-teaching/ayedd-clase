d6408f1a6119219c094eb780f69d558645c869ee
roi <rodrigo.perez@urjc.es>
2025-03-26 10:51:58 +0100

    file sorter a bit better

--- src/main/java/clase/ordenacion/FileSorter.java
+++ src/main/java/clase/ordenacion/FileSorter.java
@@ -6,11 +6,11 @@ import java.io.File;
 public class FileSorter{
 
     public static void main(String[] args){
-	File directory = new File(args[0]);
-	File[] files = directory.listFiles();
-	Insertion.sort(files);
-	for (int i = 0; i < files.length; i++)
-	    StdOut.println(files[i].getName());
+		File directory = new File(args[0]);
+		File[] files = directory.listFiles();
+		assert files != null;
+		Insertion.sort(files);
+		for (File file : files) StdOut.println(file.getName());
     }
 }
     
